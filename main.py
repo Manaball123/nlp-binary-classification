b105038bba645700ea76deab6bd77ca53ec46363
Mana <57663038+Manaball123@users.noreply.github.com>
2023-08-16 10:09:03 +0800

    transformer

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     data_parser.get_files_index()
 
     #lstm = model.LSTM(config.TOKENS_N, 64, 2048, 1)
-    lstm = model.TorchLSTM(config.WORD_SIZE * 8, config.WORD_SIZE * 8, 512, 1, 4)
+    transformer = model.TransformerModel(config.WORD_SIZE * 8, ).to(device)
     start_time = time.time()
 
 
@@ -76,8 +76,7 @@ def main():
     #train_data = data_parser.load_all_available()
     #print("Data loaded.")
 
-    #moving to gpu
-    lstm.to(device)
+    
 
     
     learning_rate = 1e-3
@@ -128,7 +127,7 @@ def main():
         random.shuffle(current_entries)
         print("\n\n\n------------------------------------Executing epoch " + str(epoch) + "------------------------------------\n\n\n")
 
-        optimizer = torch.optim.Adam(lstm.parameters(), learning_rate)
+        optimizer = torch.optim.Adam(transformer.parameters(), learning_rate)
         for it, entry in enumerate(current_entries):
             entry_start_time = time.time()
             utils.verbose_log("===========Processing " + entry + ", number " + str(it) + "============")
@@ -136,7 +135,7 @@ def main():
             target_tensor = data_parser.get_target_tensor(entry).to(device)
             utils.verbose_log("Data preprocessing complete. Target tensor: " + str(target_tensor) + ", Input tensor size: " + str(input_tensor.size()))
 
-            output, loss = train(lstm, input_tensor, target_tensor, optimizer, learning_rate, criterion)
+            output, loss = train(transformer, input_tensor, target_tensor, optimizer, learning_rate, criterion)
             current_loss += loss
             entry_end_time = time.time()
 
@@ -154,11 +153,11 @@ def main():
 
             
         learning_rate = learning_rate * 0.5
-        torch.save(lstm.state_dict(), "./checkpoints/" + session_id + "/" + str(epoch) + ".pt")
+        torch.save(transformer.state_dict(), "./checkpoints/" + session_id + "/" + str(epoch) + ".pt")
         
 
     plt.show()
-    torch.save(lstm.state_dict(), "./model.pt")
+    torch.save(transformer.state_dict(), "./model.pt")
 
     
 
--- model.py
+++ model.py
@@ -1,151 +1,62 @@
 import torch
 from torch import nn
 import utils
+import math
+from torch import Tensor
+class TransformerModel(nn.Module):
 
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
+        super().__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = nn.TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, nlayers)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, ntoken)
 
-class RNN(nn.Module):
-    def __init__(self, input_size, embedding_out_size, hidden_size, output_size):
-        super(RNN, self).__init__()
+        self.init_weights()
 
-        self.hidden_size = hidden_size
-        self.embedding_out_size = embedding_out_size
-        self.embedding = nn.Linear(input_size, embedding_out_size)
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
 
-        #input 2 hidden
-        self.i2h = nn.Linear(embedding_out_size + hidden_size, hidden_size)
-        #hidden 2 output
-        self.h2o = nn.Sequential(
-            nn.Linear(hidden_size, output_size),
-            nn.Sigmoid()
-        ) 
- 
-        self.hidden = torch.zeros(hidden_size).to(utils.get_device())
+    def forward(self, src: torch.Tensor, src_mask: torch.Tensor = None) -> torch.Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
 
-    def forward(self, input):
-        embedding_pass = self.embedding(input)
-        combined = torch.cat((embedding_pass, self.hidden), 0)
-        self.hidden = self.i2h(combined)
-        output = self.h2o(self.hidden)
-
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.linear(output)
         return output
-
-    def init_states(self):
-        self.hidden = torch.zeros(self.hidden_size).to(utils.get_device())
-        
-
-
-class LSTM(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size, output_size):
-        super(LSTM, self).__init__()
-        self.hidden_t = None
-        self.hidden_size = hidden_size
-        #AKA long term memory
-        self.c_t = None
-        self.c_size = hidden_size 
-        self.device = utils.get_device()
-        #concated embedding + hidden
-        self.intermediate_size = embedding_size + hidden_size
-        self.embedding_network = nn.Sequential(
-            #nn.Linear(input_size, embedding_size),
-            #nn.Softmax(1)
-        
-        )
-
-        self.forget_gate_network = nn.Sequential(
-            nn.Linear(self.intermediate_size, self.c_size),
-            nn.Sigmoid()
-        )
-        self.information_gate_network = nn.Sequential(
-            nn.Linear(self.intermediate_size, self.c_size),
-            nn.Sigmoid()
-        )
-        #generates the matrix that is multiplied by information
-        self.memory_signal_network = nn.Sequential(
-            nn.Linear(self.intermediate_size, self.c_size),
-            nn.Tanh()
-        )
-        #multiplied by memory(C)
-        self.hidden_output_gate_network = nn.Sequential(
-            nn.Linear(self.intermediate_size, self.hidden_size),
-            nn.Sigmoid()
-        )
-        #gets an output(finally) from the transformed hidden
-        self.hidden_to_output_network = nn.Sequential(
-            nn.Linear(hidden_size, output_size),
-            nn.Sigmoid()
-        )
-        self.memory_transformer = nn.Sequential(
-            nn.Tanh()
-        )
-      
-
-
-    def init_states(self):
-        #hidden
-        self.hidden_t = torch.zeros(self.hidden_size).to(self.device)
-        #memory cells
-        self.c_t = torch.zeros(self.c_size).to(self.device)
-
-    #data should be a onehot vector
-    def forward(self, data):
-        self.init_states()
-        #torch.set_printoptions(threshold = 10000)
-        #print(data.size())
-        embedding = self.embedding_network(data)
-        #print(embedding.size())
-        for i in range(data.size()[0]):
-            intermediates = torch.cat((embedding[i], self.hidden_t), 0)
-
-            forget_t = self.forget_gate_network(intermediates)
-            
-
-            info_t = self.information_gate_network(intermediates)
-            signal_t = self.memory_signal_network(intermediates)
-
-            info_gated_t = info_t * signal_t
-
-            #forget some info
-            self.c_t = forget_t * self.c_t
-            #add new info
-            self.c_t = torch.add(self.c_t, info_gated_t)
-
-            #transform memory for hidden
-            mem_transformed = self.memory_transformer(self.c_t)
-
-            #
-            out_gate_t = self.hidden_output_gate_network(intermediates)
-            #also selectively forget some stuff
-            self.hidden_t = mem_transformed * out_gate_t
-
-        output = self.hidden_to_output_network(self.hidden_t)
-        return output
-
-
-class TorchLSTM(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size, output_size, n_layers = 1) -> None:
-        super(TorchLSTM, self).__init__()
-        self.device = utils.get_device()
-
-        self.embedding_network = nn.Sequential(
-            nn.Embedding(embedding_size, 1)
-        )
-        #:crying_emoji:
-        self.lstm_model = nn.LSTM(input_size=embedding_size, hidden_size=hidden_size, num_layers=n_layers, batch_first=True)
-
-        self.out_newtwork = nn.Sequential(
-            nn.Linear(hidden_size, output_size),
-            nn.Sigmoid()
-        )
-        #torch.nn.init.kaiming_normal_(self.parameters())
-    
-    def forward(self, data):
-        embedding = self.embedding_network(data).squeeze(2)
-        output, (h_n, c_n) = self.lstm_model(embedding)
-        output = self.out_newtwork(output)
-        return output
-
-
     
 
+class PositionalEncoding(nn.Module):
 
-    
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
